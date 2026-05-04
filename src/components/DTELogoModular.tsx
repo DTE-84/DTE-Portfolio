49e201f344bb2ae17bc582f19250480e715adbe2
dte-84 <dte.solutions.llc@gmail.com>
2026-05-04 00:17:43 -0500

    first commit

--- src/components/DTELogoModular.tsx
+++ src/components/DTELogoModular.tsx
@@ -30,7 +30,7 @@ const DTELogoModular = forwardRef<HTMLDivElement, DTELogoModularProps>(
 
 						
 					<Image
-						src={getAssetPath("logoentry.mp4")}
+						src={getAssetPath("DTELogo.png")}
 						alt='DTE LOGO'
 						fill
 						className='object-contain'
